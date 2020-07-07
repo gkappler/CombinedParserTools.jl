c383b9b2202cce0794f3392bacdf1711f71cce5f
Gregor Kappler <gregor.kappler@bictxt.de>
2020-07-07 23:38:52 +0200

    html

--- src/html.jl
+++ src/html.jl
@@ -1,43 +1,68 @@
+export html
 
-
+# regex_neg_lookahead(e,match=AnyChar()) =
+#     !Repeat(Sequence(NegativeLookahead(e),match))
 
 export attribute_parser
+"""
+Parser matching an html attribute.
+Key is a `word` parser, value is either a
+- quoted string (escape ")
+- a `numeric%` value (without quotes)
+- a `numeric` value (without quotes)
+- a `word` value (without quotes)
+- a color `#xxxxxx` value (without quotes)
+
+`result_type` is a [`Token`](@ref).
+"""
 attribute_parser =
-    map(
-        (v) -> Token(lowercase(v[1]), intern(v[5])),
-        Token,
-        seq(
+    with_name(
+        :attribute,
+        Sequence(
             word, whitespace_maybe,"=", whitespace_maybe,
             Either(
                 Sequence(
                     2, "\"",
-                    regex_neg_lookahead("\"",re"(?:.|\\\")"),"\""),
-                re"[0-9]+%",
-                re"[-+]?[0-9]+",
-                word,
-                re"#[0-9A-Fa-f]{6}")
-        ))
+                    !!Repeat(Sequence(2,NegativeLookahead("\""),Either(tuple("\\\"",AnyChar())))),
+                    "\""),
+                !!re"[0-9]+%",
+                !!re"[-+]?[0-9]+",
+                !!word,
+                !!re"#[0-9A-Fa-f]{6}")
+        ) do v
+        Token(lowercase(v[1]), intern(v[5]))
+        end
+    )
 
-attributes = alternate(attribute_parser, whitespace_horizontal)
+attributes = join(attribute_parser, whitespace_horizontal)
 
+"""
+    html(tags=word, inner=AnyChar(), attrs=attributes)
+
+`result_type` is a [`Node`](@ref)`{eltype(result_type(attrs)),result_type(inner)}`.
+"""
 function html(tags=word, inner=AnyChar(), attrs=attributes)
     html(result_type(inner), tags, attrs) do until
         Repeat_until(inner, until)
     end
 end
 
+
+"""
+    html(inner::Function, T::Type, tags=word, attrs_parser=attributes)
+
+`result_type` is a [`Node`](@ref)`{eltype(result_type(attrs)),T}`.
+"""
 function html(inner::Function, T::Type, tags=word, attrs_parser=attributes)
     A = eltype(result_type(attrs_parser))
     function r(x,)
         (tag,attrs) = x
-        Either(map(
-            #Node{A,T},
-            (v) -> Node(tag, attrs, T[]),
-            parser("/>")),
-               Sequence(
-                   # Node{A,T},
-                   ">",
-                   inner(Sequence("</",tag,">"))) do v
+        Either(map( # Node{A,T},
+                    (v) -> Node(tag, attrs, T[]),
+                    parser("/>")),
+               Sequence( # Node{A,T},
+                         ">",
+                         inner(Sequence("</",caseless(tag),">"))) do v
                Node(tag, attrs, v[2])
                end)
     end
@@ -55,3 +80,4 @@ function html(inner::Function, T::Type, tags=word, attrs_parser=attributes)
                         whitespace_maybe)))),
         r)
 end
+
